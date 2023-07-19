da507b6723dcb68193e37bfe30287d03ba07ccab
ibrahim2375 <ibrahim.abdelrahman.abdelaziz@gmail.com>
2023-07-19 22:27:33 +0300

    gamplay data

---
+++ data/gamplay.js
@@ -0,0 +1,25 @@
+export const gameplay = [
+    {
+        id: 0,
+        title:'Third Person View',
+        description:'The Third Person View camera perspective returns from the original Resident Evil 4. Updated graphics enhance the original game\'s feeling of isolation and its haunting environments, while refined modern controls emphasize how Leon\'s survival balances on a knife\'s edge with intense gameplay.',
+        poster:'gameplay1.jpg',
+        videoCode:''
+    },
+    {
+        id: 1,
+        title:'Ranged Combat',
+        description:'Aim and shoot from Third Person View. Aim well and pick your targets carefully to conserve ammo.Make your foes falter by shooting at their heads and legs to open them up for a melee attack, and deal punishing blows without consuming ammo.',
+        poster:'gameplay2.jpg',
+        videoCode:''
+    },
+    {
+        id: 2,
+        title:'Knife Actions',
+        description:'In addition to attacking, you can now parry attacks and finish off fallen foes with your knife.Any knife action, however, will reduce its durability. Only use it when you really need to; it\'s best to save it for dire circumstances or you might be caught without a way to defend yourself.',
+        poster:'gameplay3.jpg',
+        videoCode:''
+    },
+
+    
+]
